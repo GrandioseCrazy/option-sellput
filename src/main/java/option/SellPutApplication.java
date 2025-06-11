23200f1184c89a008d80f8de703548528806020e
pengpeng <1400065562@qq,com>
2025-06-11 11:25:32 +0800

    初始化springboot项目

---
+++ src/main/java/option/SellPutApplication.java
@@ -0,0 +1,13 @@
+package option;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellPutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellPutApplication.class, args);
+    }
+
+}
